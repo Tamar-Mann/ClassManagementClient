bc73eaf6e5ee84b20095b09a4fdeef61c1ed6c59
Tamar-Man <1584tm@gmail.com>
2025-07-11 01:50:09 +0300

    change path in home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,10 +9,10 @@ const Home = () => {
       <h1>Welcome to HighClass</h1>
       <p>Please choose an action:</p>
       <div className="home-links">
-        <Link to={`/${Paths.signInTeacher}`}>🔑 Login - Teacher</Link>
-        <Link to={`/${Paths.signInStudent}`}>🔑 Login - Student</Link>
-        <Link to={`/${Paths.signUpTeacher}`}>📝 Sign Up - Teacher</Link>
-        <Link to={`/${Paths.signUpStudent}`}>📝 Sign Up - Student</Link>
+        <Link to={`${Paths.signInTeacher}`}>🔑 Login - Teacher</Link>
+        <Link to={`${Paths.signInStudent}`}>🔑 Login - Student</Link>
+        <Link to={`${Paths.signUpTeacher}`}>📝 Sign Up - Teacher</Link>
+        <Link to={`${Paths.signUpStudent}`}>📝 Sign Up - Student</Link>
       </div>
     </div>
   );
